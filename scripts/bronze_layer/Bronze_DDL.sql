115e1d152d82d28bdd3eebc9ec1cccd9d495adca
shalabyy420 <114237610+shalabyy420@users.noreply.github.com>
2026-08-31 04:18:54 +0300

    Update Bronze_DDL.sql

--- scripts/bronze_layer/Bronze_DDL.sql
+++ scripts/bronze_layer/Bronze_DDL.sql
@@ -1,1 +1,94 @@
+/*
+	DDL creation for bronze layer
+	=======================================
+	** Creating Tables of the required data with the writing rules <schema>.<source>_<table name>
 
+*/
+
+use data_warehouse
+
+-- Creating all tables of crm source 
+GO
+if OBJECT_ID ('bronze.crm_cust_info' , 'U') is not null
+	Drop table bronze.crm_cust_info;
+GO
+create table bronze.crm_cust_info 
+(
+	cst_id int,
+	cst_key nvarchar(50),
+	cst_first_name nvarchar(50),
+	cst_last_name nvarchar(50),
+	cst_marital_status nvarchar(50),
+	cst_gndr nvarchar(50),
+	cst_create_date nvarchar(50)
+);
+
+GO
+if OBJECT_ID ('bronze.crm_prd_info' , 'U') is not null
+	Drop table bronze.crm_prd_info;
+GO
+create table bronze.crm_prd_info
+(
+	prd_id int,
+	prd_key nvarchar(50),
+	prd_nm nvarchar(50),
+	prd_cost int,
+	prd_line nvarchar(50),
+	prd_start_dt datetime,
+	prd_end_dt datetime
+);
+GO
+
+if OBJECT_ID ('bronze.crm_sales_details' , 'U') is not null
+	Drop table bronze.crm_sales_details;
+GO
+Create table bronze.crm_sales_details
+(
+	sls_ord_num nvarchar(50),
+	sls_prd_key nvarchar(50),
+	sls_cust_id int,
+	sls_order_dt int,
+	sls_ship_dt int,
+	sls_due_dt int,
+	sls_sales int,
+	sls_quantity int,
+	sls_price int
+);
+GO
+-- All crm source data are done 
+
+-- creating erp source tables
+GO
+if OBJECT_ID ('bronze.erp_CUST_AZ12' , 'U') is not null
+	Drop table bronze.erp_CUST_AZ12;
+GO
+
+Create table bronze.erp_CUST_AZ12
+(
+	CID nvarchar(50),
+	BDATE date,
+	GEN nvarchar(50)
+);
+GO
+if OBJECT_ID ('bronze.erp_LOC_A101' , 'U') is not null
+	Drop table bronze.erp_LOC_A101;
+GO
+create table bronze.erp_LOC_A101
+(
+	CID nvarchar(50),
+	CNTRY nvarchar(50)
+);
+GO
+if OBJECT_ID ('bronze.erp_PX_CAT_G1V2' , 'U') is not null
+	Drop table bronze.erp_PX_CAT_G1V2;
+GO
+create table bronze.erp_PX_CAT_G1V2
+(
+	ID nvarchar(50),
+	CAT nvarchar(50),
+	SUBCAT nvarchar(50),
+	MAINTENANCE nvarchar(50)
+);
+GO
+
+-- All erp source data are done
