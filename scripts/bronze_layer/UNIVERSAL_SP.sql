b87a332793870a9c3d6e589cee3f386563ec4fd6
shalabyy420 <114237610+shalabyy420@users.noreply.github.com>
2026-08-31 04:20:11 +0300

    Add universal stored procedure for CSV loading
    Created a universal stored procedure for daily CSV file loading into specified tables.

--- scripts/bronze_layer/UNIVERSAL_SP.sql
+++ scripts/bronze_layer/UNIVERSAL_SP.sql
@@ -1,1 +1,45 @@
+/*
+  Stored procedure to do every day load of files you get
+  ------------------------------------------------------
+  this stored procedure saves so much time and effort you just give it the table name and its path and it will do the loading
 
+  NOTE:
+  this SP is UNIVERSAL so you can choose it for any files you just put file path and table name.
+*/
+
+
+CREATE OR ALTER PROCEDURE bronze.load_csv
+    @table_name NVARCHAR(128),
+    @file_path NVARCHAR(500)
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    DECLARE @sql NVARCHAR(MAX);
+
+    -- 1. Truncate the target table
+    SET @sql = N'TRUNCATE TABLE bronze.' + QUOTENAME(@table_name) + N';';
+
+    EXEC sp_executesql @sql;
+
+    -- 2. Bulk insert the CSV data
+    SET @sql = N'
+        BULK INSERT bronze.' + QUOTENAME(@table_name) + N'
+        FROM ''' + REPLACE(@file_path, '''', '''''') + N'''
+        WITH (
+            FIRSTROW = 2,
+            FIELDTERMINATOR = '','',
+            ROWTERMINATOR = ''0x0a'',
+            TABLOCK
+        );';
+
+    EXEC sp_executesql @sql;
+END;
+GO
+
+EXEC bronze.load_csv
+    @table_name = 'crm_cust_info',
+    @file_path = 'E:\datawarehouse  project\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_crm\cust_info.csv';
+GO
+
+select * from bronze.crm_cust_info
